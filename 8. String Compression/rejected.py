da638831c917415faddc59b23c2e9ec032b275dd
Fadilix <iamfadile2@gmail.com>
2024-03-22 23:17:55 +0000

    8.

---
+++ 8. String Compression/rejected.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        ans = 0
+        i = 0
+
+        while i < len(chars):
+            
+
+
+solution = Solution()
+
+print(solution.compress(["a", "a", "b", "b", "c", "c", "c"]))
